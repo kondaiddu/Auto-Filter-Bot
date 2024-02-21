32878783e81ecc15ae221a79379ff4ded9ac279b
POPCORNMINI <149669099+POPCORNMINI@users.noreply.github.com>
2024-02-21 22:07:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,8 +96,8 @@ WELCOME_TEXT = environ.get("WELCOME_TEXT", script.WELCOME_TEXT)
 INDEX_EXTENSIONS = [extensions.lower() for extensions in environ.get('INDEX_EXTENSIONS', 'mp4 mkv').split()]
 
 # boolean settings 
-GROUP_FSUB = is_enabled('GROUP_FSUB', False) 
-PM_SEARCH = is_enabled('PM_SEARCH', False) #switch True or False for searching results in bot pm😃
+GROUP_FSUB = is_enabled('GROUP_FSUB', True) 
+PM_SEARCH = is_enabled('PM_SEARCH', True) #switch True or False for searching results in bot pm😃
 IS_VERIFY = is_enabled('IS_VERIFY', False)
 AUTO_DELETE = is_enabled('AUTO_DELETE', True)
 WELCOME = is_enabled('WELCOME', True)
