8b51ce15d2991a8c90e7829592cf7308c099ee53
POPCORNMINI <149669099+POPCORNMINI@users.noreply.github.com>
2024-02-22 12:15:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -97,7 +97,7 @@ INDEX_EXTENSIONS = [extensions.lower() for extensions in environ.get('INDEX_EXTE
 
 # boolean settings 
 GROUP_FSUB = is_enabled('GROUP_FSUB', True) 
-PM_SEARCH = is_enabled('PM_SEARCH', True) #switch True or False for searching results in bot pm😃
+PM_SEARCH = is_enabled('PM_SEARCH', False) #switch True or False for searching results in bot pm😃
 IS_VERIFY = is_enabled('IS_VERIFY', False)
 AUTO_DELETE = is_enabled('AUTO_DELETE', True)
 WELCOME = is_enabled('WELCOME', True)
