8bc6ec9a454234471fd7e91e8958161256b1761c
POPCORNMINI <149669099+POPCORNMINI@users.noreply.github.com>
2024-03-07 14:41:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "https://dashboard.heroku.com/apps/tmafiles/") #if heroku then paste the app link here ex: https://heroku......./
+URL = environ.get("URL", "https://tmafiles-de5273d4544e.herokuapp.com") #if heroku then paste the app link here ex: https://heroku......./
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
